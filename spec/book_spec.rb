d6bbf4c8cbe05076be751fb51eb4ec35d8522ea0
Tackeyyyyyyyy <meteoreon@yahoo.co.jp>
2018-09-16 21:06:21 +0900

    test add

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Book do
     expect(book.name).to eq("test book")
   end
 
+  it "name is test book" do
+    book2 = Book2.new
+    expect(book2.name).to eq("test book2")
+  end
+
 end
